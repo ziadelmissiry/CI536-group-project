4dc4182bc5c497a0eff131abc88116c016dcd1b5
ziadelmissiry <ziadelmissiry@hotmail.com>
2023-03-17 21:49:45 +0000

    push

--- script.js
+++ script.js
@@ -1,59 +1,75 @@
-// Get the necessary DOM elements
-const userInput = document.getElementById('user-input');
-const submitButton = document.getElementById('submit-button');
-const chatOutput = document.getElementById('chat-output');
+// // Get the necessary DOM elements
+// const userInput = document.getElementById('user-input');
+// const submitButton = document.getElementById('submit-button');
+// const chatOutput = document.getElementById('chat-output');
 
-// Function to handle the user's message
-function handleUserMessage() {
-  // Get the user's message from the input field
-  const userMessage = userInput.value;
+// // Function to handle the user's message
+// function handleUserMessage() {
+//   // Get the user's message from the input field
+//   const userMessage = userInput.value;
 
-  // You can add your chatbot's response logic here
-  const chatbotResponse = `You said: ${userMessage}`;
+//   // You can add your chatbot's response logic here
+//   const chatbotResponse = `You said: ${userMessage}`;
 
-  // Display the chatbot's response in the output area
-  chatOutput.innerHTML += `<p>${chatbotResponse}</p>`;
+//   // Display the chatbot's response in the output area
+//   chatOutput.innerHTML += `<p>${chatbotResponse}</p>`;
 
-  // Clear the user input field
-  userInput.value = '';
-}
+//   // Clear the user input field
+//   userInput.value = '';
+// }
 
-// Add an event listener to the submit button
-submitButton.addEventListener('click', handleUserMessage);
+// // Add an event listener to the submit button
+// submitButton.addEventListener('click', handleUserMessage);
 
 
 
 
 //// chat bot 
 
-const chatDisplay = document.getElementById('chat-display');
-const chatInput = document.getElementById('chat-input');
+document.addEventListener('DOMContentLoaded', () => {
+  const userInput = document.getElementById('user-input');
+  const sendButton = document.getElementById('send-button');
+  const chatOutput = document.getElementById('chat-output');
 
-chatInput.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
-    const message = chatInput.value.trim().toLowerCase();
-    chatInput.value = '';
+  sendButton.addEventListener('click', () => {
+      const input = userInput.value;
+      userInput.value = '';
+      sendMessage(input, 'user-message');
+      generateResponse(input);
+  });
 
-    if (message === 'hello') {
-      appendBotMessage('Hello back!');
-    } else {
-      appendUserMessage(message);
-      appendBotMessage("I'm sorry, I didn't understand that.");
-    }
+  userInput.addEventListener('keypress', (event) => {
+      if (event.key === 'Enter') {
+          const input = userInput.value;
+          userInput.value = '';
+          sendMessage(input, 'user-message');
+          generateResponse(input);
+      }
+  });
+
+  function sendMessage(text, className) {
+      if (text.trim() !== '') {
+          const messageElement = document.createElement('div');
+          messageElement.classList.add('message', className);
+          messageElement.textContent = text;
+          chatOutput.appendChild(messageElement);
+          chatOutput.scrollTop = chatOutput.scrollHeight;
+      }
+  }
+
+  function generateResponse(userInput) {
+      let response = '';
+
+      if (userInput.toLowerCase() === 'hello world'.toLowerCase()) {
+          response = 'Hello Ziad';
+      } else {
+          response = 'I am not programmed to respond to this message.';
+      }
+
+      // You can add more conditions here
+
+      setTimeout(() => {
+          sendMessage(response, 'bot-message');
+      }, 1000);
   }
 });
-
-function appendUserMessage(message) {
-  const userMessage = document.createElement('div');
-  userMessage.classList.add('user-message');
-  userMessage.textContent = message;
-  chatDisplay.appendChild(userMessage);
-}
-
-function appendBotMessage(message) {
-  const botMessage = document.createElement('div');
-  botMessage.classList.add('bot-message');
-  botMessage.textContent = message;
-  chatDisplay.appendChild(botMessage);
-}
-
